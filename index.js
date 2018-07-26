49758c4dfde25e6c9e450b9a67e1511f024788cc
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-07-26 21:18:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,3 +40,20 @@ var pageData = {
   }
   
  }
+
+var app = express()
+  // app.use(express.static(path.join(__dirname, 'public')))
+  app.use('/static', express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+
+// ---------------------------------
+// HOME	
+
+app.get('/home', (req, res) => res.render(index,pageData.home))
+
+// ----------------------------------
+// ----------------------------------
+// ----------------------------------
+
+ app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
