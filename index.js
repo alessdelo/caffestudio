cecff255d79eab39ac3a756d38f760e3eadeff97
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-08-17 18:14:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
  
 app.get('/', function (req, res) {
   res.send('Hello World!')
