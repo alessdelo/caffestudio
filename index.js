a7cb10e9d7ab72e6bca088768c0f92e7805bd5d8
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-08-18 15:32:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var express = require('express')
-var app = express()
-const path = require('path')
-
-const PORT = process.env.PORT || 5000
- 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
- 
-
- app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
