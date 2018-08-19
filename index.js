bb997663b115111832abfe4bbdbeeba263c93614
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-08-19 12:00:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+const assert = require('assert')
+const bodyParser = require('body-parser')
+const THREE = require('three')
+
 const PORT = process.env.PORT || 5000
 
 express()
